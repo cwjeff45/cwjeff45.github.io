a6c6e8814a5d4b07b6f75e0382e95595c2ca611c
Colten Jeffress <cwjeffress@gmail.com>
2025-07-14 19:09:10 -0500

    Fixed 'error' text

--- secret-reveal.js
+++ secret-reveal.js
@@ -1,8 +1,10 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const encoded = "Q29kZTogVU5JVFk=";
+    const encoded = "Y29tcGxldGU="; // base64 for "complete"
     const decoded = atob(encoded);
 
     const secretEl = document.getElementById("secretCode");
-    secretEl.textContent = decoded;
+    const defaultSpan = secretEl.querySelector(".default-text");
+
+    defaultSpan.textContent = decoded;
     secretEl.style.display = "block";
 });
